b5905392466647aeea0d28949f768eea480b567c
RobertJN64 <robertjnies@gmail.com>
2023-08-02 18:06:14 -0400

    find other tables

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pandas as pd
 import tabula
 import json
 
+def parse_table_v2(table, p_list):
+    for _, row in table.iterrows():
+        param = row[0]
+        if isinstance(param, float):
+            continue
+        p_list.append(param)
+
 def parse_table(table, p_list):
     for _, row in table.iterrows():
         param = row[0]
@@ -19,20 +26,24 @@ def parse_table(table, p_list):
 def main():
     pdf_path = "HYN001 protocol.pdf"
 
-    dfs: list[pd.DataFrame] = tabula.read_pdf(pdf_path, stream=True, pages='52-105', pandas_options={'header': None})
+    dfs: list[pd.DataFrame] = tabula.read_pdf(pdf_path, stream=True, pages='51-105', pandas_options={'header': None})
     # read_pdf returns list of DataFrames
     last_valid_p_list = []
     j = {}
     for counter, table in enumerate(dfs):
         table: pd.DataFrame = table
         p_list = []
-        if table[0][0] == 'Parameter':
-            parse_table(table[1:], p_list)
+        # if table[0][0] == 'Parameter':
+        #     parse_table(table[1:], p_list)
+        #     j['GTXX' + str(counter)] = p_list
+        #     last_valid_p_list = p_list
+        # else:
+        #     if len(table.columns) == 4:
+        #         parse_table(table, last_valid_p_list)
+
+        if len(table.columns) != 4:
+            parse_table_v2(table, p_list)
             j['GTXX' + str(counter)] = p_list
-            last_valid_p_list = p_list
-        else:
-            if len(table.columns) == 4:
-                parse_table(table, last_valid_p_list)
 
     with open('tables_out.json', 'w+') as f:
         json.dump(j, f, indent=4)
